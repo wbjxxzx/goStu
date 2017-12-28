7fa2a8a75d8e11151a3f0adc0d1245384c15150b
wbjxxzx <wbjxxzx@163.com>
2017-12-28 09:01:52 +0800

    add cmpString

---
+++ ch3/cmpString.go
@@ -0,0 +1,32 @@
+// compare tow strings have the same char, even they do not hav the same order
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(cmpString("abc", "bca"))
+	fmt.Println(cmpString("aabbc", "baabc"))
+	fmt.Println(cmpString("abcde", "abcd"))
+	fmt.Println((cmpString("ab中国同同国a", "中国a国同ba同")))
+}
+func cmpString(a, b string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	ma := make(map[rune]int)
+	mb := make(map[rune]int)
+	for _, v := range a {
+		ma[v]++
+	}
+	for _, v := range b {
+		mb[v]++
+	}
+	for k, v := range ma {
+		if mb[k] != v {
+			return false
+		}
+	}
+	return true
+}
